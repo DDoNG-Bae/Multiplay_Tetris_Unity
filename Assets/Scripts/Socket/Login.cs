3755bb0dbe9b1de07f1ecb972cdaa02915152348
Dasom Lim <ektha03@gmail.com>
2018-01-18 20:06:18 +0900

    TEST commit

--- Assets/Scripts/Socket/Login.cs
+++ Assets/Scripts/Socket/Login.cs
@@ -1,16 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using socket.io;
+using UnityEngine.UI;
 public class Login : MonoBehaviour {
+    public InputField idInput;
+    public InputField passInput;
 
+    Socket socket;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        socket = Socket.Connect("http://ec2-52-78-8-84.ap-northeast-2.compute.amazonaws.com/");
+        
+        socket.On("loginResult",(string res)=>{
+            Debug.Log(res);
+        });
+
+    }
+
+    public void login()
+    {
+        User2 user = new User2();
+        user.id = idInput.text;
+        user.pass = passInput.text;
+
+        socket.EmitJson("login",JsonUtility.ToJson(user));
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+}
+
+public class User2
+{
+    public string id;
+    public string pass;
 }
