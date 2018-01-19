9106f629bca6940c570b7c41d661d8ec964f32d9
palewalker <lucian0@naver.com>
2018-01-19 23:19:37 +0900

    no message

--- Assets/Scripts/MultiLobbyScene.cs
+++ Assets/Scripts/MultiLobbyScene.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class MultiLobbyScene : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Room[] mRoomList;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void EnterRoom(Room tRoom)
+    {
+
+    }
+
+    public class Room
+    {
+        int mPlayerNum;
+        string mRoomName;
+
+    }
 }
