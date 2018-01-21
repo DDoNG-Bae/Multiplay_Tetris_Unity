7378686c9c6cf5de90d6616de55755f2576d6f3b
Dasom Lim <ektha03@gmail.com>
2018-01-22 02:09:36 +0900

    Edit login socket url

--- Assets/Scripts/Socket/Login.cs
+++ Assets/Scripts/Socket/Login.cs
@@ -11,7 +11,7 @@ public class Login : MonoBehaviour {
     Socket socket;
 	// Use this for initialization
 	void Start () {
-        socket = Socket.Connect("http://ec2-52-78-8-84.ap-northeast-2.compute.amazonaws.com:3000"+"/login");
+        socket = Socket.Connect("http://ec2-52-78-8-84.ap-northeast-2.compute.amazonaws.com:3000");
         
         /*
          * res 
